ca261eb81ecf4b9dfff36138d14cc5a1ec7e6947
Dmitrii <shokilla@mail.ru>
2021-11-04 12:47:25 +0300

    finish productComponent

--- public/js/productsComp.js
+++ public/js/productsComp.js
@@ -6,7 +6,39 @@ Vue.component('products', {
       }
    },
    methods: {
+      filter(value) {
+         let regexp = new RegExp(value, 'i');
+         this.filtered = this.products.filter(el => regexp.test(el.product_name));
+      }
+   },
 
-   }
+   mounted() {
+      this.$parent.getJson('/api/products')
+         .then(data => {
+            for (let elem of data) {
+               this.products.push(elem);
+               this.filtered.push(elem);
+            }
+         })
+   },
 
+   template: `<div class="products">
+      <product
+      v-for="product of filtered"
+      :product="product"
+      :img="product.imgProduct"></product>
+      </div>`
+
+});
+
+Vue.component('product', {
+   props: ['product', 'img'],
+   template: `<div class='product-item>
+         <img :src='img' :alt='product.product_name'>
+         <div class='desc'>
+         <h3>{{ product.product_name }}</h3>
+         <div><p>{{ product.price }}</p></div>
+         <button class='buy-btn' @click="$parent$refs.cart.addProduct(product)">Купить</button>
+         </div>
+   </div>`
 })
